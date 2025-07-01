9731885acaea350171122e3d4bd949b812285e75
Nikk434 <159627869+Nikk434@users.noreply.github.com>
2025-07-01 14:18:53 +0530

    updated url

--- frontend/aqua-analyzer-frontend/src/app/api/video/route.js
+++ frontend/aqua-analyzer-frontend/src/app/api/video/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
-  const response = await fetch('https://aqua-analyzer-backend.nllnwr.easypanel.host/video_feed');
+  const response = await fetch('https://aquaanalyzer.onrender.com/video_feed');
   return new Response(response.body, {
     headers: {
       'Content-Type': 'multipart/x-mixed-replace; boundary=frame',
